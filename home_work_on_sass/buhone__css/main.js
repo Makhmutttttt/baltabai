e76d6c57e431e8d0f8b8fa7bd92f965eae2b414b
Makhmutttttt <mahmutbaltabaj@gmail.com>
2023-02-18 11:02:37 +0600

    clean__calk

--- home_work_on_sass/buhone__css/main.js
+++ home_work_on_sass/buhone__css/main.js
@@ -5,51 +5,43 @@ let work;
 let arr = [];
 let counter = 0;
 let text;
+let ansver;
+function calk(){
+    for(let t = 1;t>0;t++){
+        work = confirm("continue or break");
+        if(work === true){
+            num1 = Number(prompt("Enter__first__number"));
+            op = String(prompt("Enter__operation"));
+            num2 = Number(prompt("Enter__second__number"));
+            if(op == "+"){
+                ansver = num1 + num2;
+            }
+            else if(op == "-"){
+                ansver = num1 - num2;
+            }
+            else if(op == "*"){
+                ansver = num1 * num2;
+            }
+            else if(op == "/"){
+                ansver = num1 / num2;
+            }
 
+            arr.push(num1 + op + num2 + "=" + ansver.toString());
 
-
-for(let t = 1;t>0;t++){
-    work = confirm("continue or break");
-
-    if(work === true){
-        num1 = Number(prompt("Enter__first__nubber"));
-        op = String(prompt("Enter__operation"));
-        num2 = Number(prompt("Enter__second__nubber"));
-        if(op == "+"){
-            ansver = num1 + num2;
+            alert(ansver);
+            counter++;
         }
-        else if(op == "-"){
-            ansver = num1 - num2;
+        else if (work === false){
+            for(let i = 0;i < counter;i++){
+                alert(arr[i]);
+            }
+            arr.length = 0;
+            break;
         }
-        else if(op == "*"){
-            ansver = num1 * num2;
-        }
-        else if(op == "/"){
-            ansver = num1 / num2;
-        }
-        // text = num1 + op
-        arr.push(num1 + op + num2 + "=" + ansver.toString());
 
-        alert(ansver);
-        counter++;
     }
-    else if (work === false){
-        for(let i = 0;i < counter;i++){
-            alert(arr[i]);
-        }
-        arr.length = 0;
-        break;
-    }
-    
-
-
-   
-
-   
-
 }
-
-
+calk();
 
 
 
